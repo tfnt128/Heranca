5a223b5a1f44da4342de918c3febbc88cbbe9c36
thiagoatf@hotmail.com <thiagoatf@hotmail.com>
2021-10-14 07:30:10 -0300

    Conta poupança finalizada

---
+++ Herança/Entities/Poupanca.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herança
+{
+    class Poupanca : Conta
+    {
+        //Variáveis e uso do encapsulamento
+        public double TaxaJuros { get; set; }
+
+        //Construtor padrão
+        public Poupanca()
+        {
+        }
+
+        //Construtor com parâmetros utilizando a Classe Conta
+        public Poupanca(int numero, string titular, double saldo, double taxajuros) 
+            : base(numero, titular, saldo)
+        {
+            TaxaJuros = taxajuros;
+        }
+
+        //Método de atualização de saldo
+        public void atualizacaoSaldo()
+        {
+            Saldo += Saldo * TaxaJuros;
+        }
+    }
+}
