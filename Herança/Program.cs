de6d62c8f0b0b8b295e556b1edf3f2c0726f7587
thiagoatf@hotmail.com <thiagoatf@hotmail.com>
2021-10-20 19:55:08 -0300

    h

--- Herança/Program.cs
+++ Herança/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace Herança
 {
@@ -6,9 +8,29 @@ namespace Herança
     {
         static void Main(string[] args)
         {
-            ContaJuridica conta = new ContaJuridica(8010, "LaraCroft", 100.0, 500.0);
+            //criando lista da classe "conta" e instancindo na lista
+            List<Conta> list = new List<Conta>();
+            list.Add(new Poupanca(1001, "Terry", 500.0, 0.01));
+            list.Add(new ContaJuridica(1002, "Andy", 500.0, 400.0));
+            list.Add(new Poupanca(1003, "Joe", 500.0, 0.01));
+            list.Add(new ContaJuridica(1002, "Mae", 500.0, 400.0));
 
-            Console.WriteLine(conta.Saldo);
+            double soma = 0.0;
+
+            foreach (Conta acc in list)
+            {
+                soma += acc.Saldo;
+            }
+            Console.WriteLine("Total do saldo de todas as contas: " + soma.ToString("F2", CultureInfo.InvariantCulture));
+            foreach (Conta acc in list)
+            {
+                acc.Saque(10.0);
+            }
+            foreach (Conta acc in list)
+            {
+                Console.WriteLine("Atualização do saldo das contas " + acc.Numero + ": " + acc.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+                
+            }
 
         }
     }
